84f54076d056a42b67c5a0ca4a41a8ac2fbfe814
Nihal <nihalsathawane2003@gmail.com>
2023-12-12 00:50:18 +0530

    OTP OOP version3

--- oopv1.py
+++ oopv1.py
@@ -1,37 +1,21 @@
 import random
 import smtplib
 from twilio.rest import Client
-  # Assuming keys.py contains necessary Twilio credentials
 
 class OTPGenerator:
-    def __init__(self, account_sid, auth_token, twilio_number):
-        self.account_sid = account_sid
-        self.auth_token = auth_token
-        self.twilio_number=twilio_number
+    def __init__(self, sender):
+        self.sender = sender
 
     def generate_otp(self):
         """Generate a random OTP."""
         return ''.join([str(random.randint(0, 9)) for _ in range(4)])
 
-    def send_otp_over_email(self, email):
-        """Send OTP over email to the specified email address."""
-        if self.validate_email(email):
-            # Creating a collaboration with SmtpServer class for email sending
-            smtp_server = SmtpServer('smtp.gmail.com', 587, 'hanumanj2k3@gmail.com', 'iqvuzodoqtsvlfks')
-            smtp_server.send_email(email, self.generate_otp())
-            print(f"OTP is sent to {email} via email.")
+    def send_otp(self, recipient, message):
+        """Send OTP using the specified sender."""
+        if self.sender.validate(recipient):
+            self.sender.send_otp(recipient, self.generate_otp(), message)
         else:
-            print("Invalid email address.")
-
-    def send_otp_over_mobile(self, mobile):
-        """Send OTP over mobile to the specified mobile number."""
-        if self.validate_mobile(mobile):
-            # Creating a collaboration with TwilioClient class for SMS sending
-            twilio_client = TwilioClient(self.account_sid, self.auth_token,self.twilio_number)
-            twilio_client.send_sms(mobile, self.generate_otp())
-            print(f"OTP is sent to {mobile} via mobile.")
-        else:
-            print("Invalid mobile number.")
+            print(f"Invalid {self.sender.name.lower()} address.")
 
     @staticmethod
     def validate_email(email):
@@ -44,50 +28,66 @@ class OTPGenerator:
         return len(mobile) == 10 and mobile.isdigit()
 
 
-class SmtpServer:
+class CommunicationService:
+    def validate(self, identifier):
+        raise NotImplementedError("Subclasses must implement this method.")
+
+    def send_otp(self, recipient, otp, message):
+        raise NotImplementedError("Subclasses must implement this method.")
+
+
+class SmtpServer(CommunicationService):
     def __init__(self, host, port, username, password):
         self.host = host
         self.port = port
         self.username = username
         self.password = password
+        self.name = "Email"
 
-    def send_email(self, recipient, otp):
+    def validate(self, email):
+        return OTPGenerator.validate_email(email)
+
+    def send_otp(self, recipient, otp, message):
         """Send an email with the OTP using the SMTP server."""
         server = smtplib.SMTP(self.host, self.port)
         server.starttls()
         server.login(self.username, self.password)
-        msg = f'Hello, your OTP is {otp}\nPlease do not share the OTP with anyone.\n' \
-              'This is a system-generated mail, so do not reply.'
+        msg = f'Hello, your OTP is {otp}\n{message}'
         server.sendmail(self.username, recipient, msg)
         server.quit()
 
 
-class TwilioClient:
+class TwilioClient(CommunicationService):
     def __init__(self, account_sid, auth_token, twilio_number):
         self.client = Client(account_sid, auth_token)
         self.twilio_number = twilio_number
+        self.name = "SMS"
 
-    def send_sms(self, recipient, otp):
+    def validate(self, mobile):
+        return OTPGenerator.validate_mobile(mobile)
+
+    def send_otp(self, recipient, otp, message):
         """Send an SMS with the OTP using the Twilio client."""
         message = self.client.messages.create(
-            body=f'Hello, your OTP is {otp}\nPlease do not share the OTP with anyone.\n'
-                 'This is a system-generated message, so do not reply.',
+            body=f'Hello, your OTP is {otp}\n{message}',
             from_=self.twilio_number,
             to=recipient
         )
 
-
-
 if __name__ == "__main__":
-    account_sid = 'AC87d29f1bf3374f2067a057ddd8d8dd5e'
-    auth_token = '50f3a51a3c806ed0a06f7fe95100efa4'
+    account_sid = 'AC32e472ca099fb7da6e850f8f9dd54e17'
+    auth_token = 'e391f32bd79bd5b6e9094cc7f8db5f89'
+    twilio_number = '+15132179720'
+    target_number = '+917875808821'
 
-    twilio_number = '+14705163287'
-    target_number = '+919552650041'
-    otp_generator = OTPGenerator(account_sid,auth_token,twilio_number)
+    smtp_server = SmtpServer('smtp.gmail.com', 587, 'hanumanj2k3@gmail.com', 'iqvuzodoqtsvlfks')
+    twilio_client = TwilioClient(account_sid, auth_token, twilio_number)
+
+    otp_generator_smtp = OTPGenerator(smtp_server)
+    otp_generator_twilio = OTPGenerator(twilio_client)
 
     email_address = "nihalsathawane2003@gmail.com"
-    otp_generator.send_otp_over_email(email_address)
+    otp_generator_smtp.send_otp(email_address, 'Please do not share the OTP with anyone.\nThis is a system-generated mail, so do not reply.')
 
-    mobile_number = "9552650041"
-    otp_generator.send_otp_over_mobile(mobile_number)
+    mobile_number = "7875808821"
+    otp_generator_twilio.send_otp(mobile_number, 'Please do not share the OTP with anyone.\nThis is a system-generated message, so do not reply.')
